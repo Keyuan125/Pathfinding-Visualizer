c3e45381c4374aa196e214f26f2dd4b46f427702
Keyuan Huang <keyuanhuang@Keyuans-MacBook-Pro-2.local>
2021-06-10 21:27:54 -0600

    finished the basic functionality

--- src/PathfindingVisualizer/PathfindingVisualizer.js
+++ src/PathfindingVisualizer/PathfindingVisualizer.js
@@ -6,52 +6,97 @@ import {dijkstra, getPath} from '../algorithms/dijkstra';
 
 const DEFAULT_START_ROW = 5;
 const DEFAULT_START_COL = 10;
-const DEFAULT_TARGET_ROW = 5;
-const DEFAULT_TARGET_COL = 40;
+const DEFAULT_TARGET_ROW = 4;
+const DEFAULT_TARGET_COL = 36;
 
 export default class PathfindingVisualizer extends Component {
   constructor() {
     super();
     this.state = {
       grid: [],
+      mouseIsPressed: false,
     };
   }
 
   // Naively create a 20X50 grid on the screen
   componentDidMount() {
     const grid = initializeGrid();
+
     this.setState({grid});
+  }
+
+
+  animateDijkstra(visitedNodes, path) {
+    for (let i = 1; i < visitedNodes.length; i++) {
+      // If we didn't reach the end of the list,
+      // then we keep rendering the visted nodes
+      if (i !== visitedNodes.length-1) {
+        setTimeout(() => {
+          const node = visitedNodes[i];
+          document.getElementById(`node-${node.row}-${node.col}`).className = 'node node-visited';
+        }, 10 * i);
+      }
+
+      // If we reach the end of the array, then we'll also animate the shortest path
+      else {
+        setTimeout(() => {
+          this.animateShortestPath(path)} , 10 * i);
+      }
+    }
+  }
+
+  animateShortestPath(path) {
+    for (let i = 1; i < path.length-1; i++) {
+      console.log(path[i]);
+      setTimeout(() => {
+        const node = path[i];
+        document.getElementById(`node-${node.row}-${node.col}`).className='node node-shortest-path';}, 50*i);
+    }
+  }
+
+
+  visualizeDijkstra() {
+    const {grid} = this.state;
 
     const startNode = grid[DEFAULT_START_ROW][DEFAULT_START_COL];
     const finishNode = grid[DEFAULT_TARGET_ROW][DEFAULT_TARGET_COL];
 
     const visited = dijkstra(grid, startNode, finishNode);
     const path = getPath(finishNode);
-    console.log(path);
+    this.animateDijkstra(visited, path);
   }
 
+
   render() {
-    const {grid} = this.state;
+    const {grid, mouseIsPressed} = this.state;
+
 
     return (
+      <>
+        <button onClick={() => this.visualizeDijkstra()}>
+          Visualize Dijkstra's Algorithm
+        </button>
       <div className="grid">
         {grid.map((row, rowIdx) => {
           return (
             <div key={rowIdx}>
               {row.map((node, nodeIdx) => {
-                const {isStart, isFinish} = node;
+                const {row, col, isStart, isFinish, isWall} = node;
                 return (
                   <Node
                     key={nodeIdx}
+                    row={row}
+                    col={col}
                     isStart={isStart}
                     isFinish={isFinish}
-                    test={'foo'}></Node>
+                    isWall={isWall}></Node>
                 );
               })}
               </div>
           );
         })}
       </div>
+    </>
     );
   }
 }
@@ -62,12 +107,16 @@ const initializeGrid = () => {
   for (let row = 0; row < 20; row++) {
     const currentRow = [];
     for (let col = 0; col < 50; col++) {
-      currentRow.push(createNode(row, col));
+      let newNode = createNode(row,col);
+      if (col > 12 && col < 26 && row < 19) newNode.isWall = true;
+      currentRow.push(newNode);
+
     }
     grid.push(currentRow);
   }
   return grid;
 };
+
 
 
 // A method to create the node
